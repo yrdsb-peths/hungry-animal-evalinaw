f38d2a762bbbe5cb31f467e02637cbba855bc09d
evalinaw <117936865+evalinaw@users.noreply.github.com>
2022-12-01 12:52:13 -0500

    Moved and centered

---
+++ hungry animals/MyWorld.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The World our hero lives in.
+ * 
+ * @author Wang
+ * @version 11/30/22
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+    }
+}
