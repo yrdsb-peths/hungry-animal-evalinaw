d8042152cb88baa4df9b6aa6d71f51dadcdb273d
evalinaw <117936865+evalinaw@users.noreply.github.com>
2022-12-11 23:13:26 -0500

    Randomize apple location

--- MyWorld.java
+++ MyWorld.java
@@ -18,10 +18,19 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 300);
+        addObject(elephant, 300, 200);
         
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random locations
+     */
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
-        
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
