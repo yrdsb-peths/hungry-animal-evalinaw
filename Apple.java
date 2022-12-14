cd015311bd204c9a4de0125da64b16e0911b0379
evalinaw <117936865+evalinaw@users.noreply.github.com>
2022-12-13 23:15:46 -0500

    Levels

--- Apple.java
+++ Apple.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
+    int speed = 1;
+    
     /**
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,7 +17,9 @@ public class Apple extends Actor
     public void act()
     {
         // Add your action code here.
-        setLocation(getX(), getY() + 1);
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
         
         // Remove apple and draw game over when apple gets to bottom
         MyWorld world = (MyWorld) getWorld();
@@ -24,5 +28,10 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -3,12 +3,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Title Screen.
  * 
- * @author Wang 
+ * @author Wang
  * @version December 2022
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 1600);
+    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -19,5 +19,18 @@ public class TitleScreen extends World
         super(600, 400, 1); 
         
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+        // Start the game if user presses the space bar.
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
